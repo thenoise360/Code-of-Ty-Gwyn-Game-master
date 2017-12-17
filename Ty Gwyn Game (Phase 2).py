c722c04d752ee17754e11b709eac554fe8e008a9
thenoise360 <34424077+thenoise360@users.noreply.github.com>
2017-12-17 20:07:28 +0000

    Add files via upload

--- Ty Gwyn Game (Phase 2).py
+++ Ty Gwyn Game (Phase 2).py
@@ -1,8 +1,103 @@
+#HEADER - Defining functions
+
+showInstructions = ("|<===========================================>|\n\nWelcome to the Ty Gwyn night out adventure\nCommands:\n Type \"Go To\" and one of the objects in the room = (e.g. desk, wardrobe, bed, sink, shelf, door)\nget [item]\n\nMISSON: Leave in the taxi to go on a famous Ty Gwyn night out in Cardiff.\n\n|<===========================================>|\n")
+
+def desk():
+	print ("You are standing at the desk. On the desk you can see your wallet.\nYou remember leaving your phone here but it doesn't appear to be on the surface top.\nIt must be in the area but you'll have to look.")
+	action = input("\nWhere would you like to search for your phone?\n-->").lower()
+	#This refers to "Exit Desk" = ed. I will use this format for all of the exit codes (as its a little easier to use)	
+	
+	ed = 1
+	while (ed ==1 ):
+		if action == "top drawer":
+			action = input("You look into the top drawer and it appears empty.\nYou will need to continue searching!\n-->").lower()
+		elif action == "bottom drawer":
+			action = input("You look into the bottom drawer and it appears empty.\nYou will need to continue searching!\n-->").lower()
+		elif action == "under the desk":
+			ed = ed - 1
+			print ("It appears your phone must have fallen on the floor.\nIt is added to your inventory")
+			break
+		elif action == "behind the desk":
+			action = input("You look behind the desk and don\'t see your phone.\nYou will need to continue searching!\n-->").lower()
+		elif action == "help":
+			print (showInstructions)
+			action = input("When you have read the instructions, keep looking! \n-->").lower()
+		elif action == "hint":
+			print ("Here you will need to look under the desk, behind the desk, and in the top and bottom drawer to find what you are looking for.")
+			action = input("When you are ready to keep looking, try typing in one of the instructions shown! \n-->").lower()
+		elif action == "go back":
+			bedStart()
+			break
+		elif action == "go to desk":
+			desk()
+			break
+		elif action == "go to bed":
+			bed()
+			break
+		elif action == "go to start":
+			bedStart()
+			break
+		elif action == "go to sink":
+			sink()
+			break
+		elif action == "go to shelf":
+			shelf()
+			break
+		elif action == "go to wardrobe":
+			wardrobe()
+			break
+		elif action == "go to door":
+			door()
+			break
+		else: action=input("It doesn't seem like you are looking in the right places, something else, or type \'hint\' if you get stuck! \n-->").lower()
+
+
+def bedStart():
+	print ("You are in your room ready to go on a night out.\nYou are sat on your bed and look around your room.\nIn front of you, you can see your desk. It has 2 draws.\nThere is a book shelf with books in and an empty beer bottle.\nYou look towards your left and you can see that the window is closed.\nTo your right is you wardrobe which is closed, and that your sink is empty.\nOn the other side of the wardrobe is the door out to the communal hallway.\n")
+	action = input("\nWhere would you like to go next?\n-->").lower()
+	
+	#This refers to "Exit Bed" = eb. I will use this format for all of the exit codes (as its a little easier to use)
+	
+	eb = 1
+	while (eb ==1 ):
+		if action == "go to desk":
+			desk()
+			break
+		elif action == "go to bed":
+			bed()
+			break
+		elif action == "go to start":
+			bedStart()
+			break
+		elif action == "go to sink":
+			sink()
+			break
+		elif action == "go to shelf":
+			shelf()
+			break
+		elif action == "go to wardrobe":
+			wardrobe()
+			break
+		elif action == "go to door":
+			door()
+			break
+		elif action == "help":
+			print (showInstructions)
+			action = input("When you have read the instructions, keep looking! \n-->").lower()
+		elif action == "hint":
+			print ("Here you want to pick one of the locations listed in the original dialogue.\nYou can see it here:\n\nYou are in your room ready to go on a night out.\nYou are sat on your bed and look around your room.\nIn front of you, you can see your desk. It has 2 draws.\nThere is a book shelf with books in and an empty beer bottle.\nYou look towards your left and you can see that the window is closed.\nTo your right is you wardrobe which is closed, and that your sink is empty.\nOn the other side of the wardrobe is the door out to the communal hallway.\n")
+			action = input("When you have decided where to go, type below and hit [enter]\n-->").lower()
+		else: action = input("That isn\'t a location in the room, try again!\n-->").lower()
+		
+
+		
+				
+		
+#End of Header ---------------------------------------------------------------------------->
+
 e1 = 1
 
 print ("\nWelcome to the Ty Gwyn night out adventure!\n\nIf you ever get stuck, just type \'help\' to get a list of instructions, or \'hint\' to get a small hint on the puzzle in front of you.\n\nEnjoy!\n\n - Jack and Tom\n\n|<===========================================>|\n")
-
-showInstructions = ("|<===========================================>|\n\nWelcome to the Ty Gwyn night out adventure\nCommands:\ngo [direction] = (north, south, east, west)\nget [item]\n\nMISSON: Leave in the taxi to go on a famous Ty Gwyn night out in Cardiff.\n\n|<===========================================>|\n")
 
 def Player():
     #Ask the user their name to determine starting point
@@ -63,15 +158,9 @@ elif playerName == "tom":
     currentRoom = 2
 else: currentRoom = 6
 
+bedStart()
 
-print ("You are in your room ready to go on a night out.\nYou are sat on your bed and look around your room.\nIn front of you, you can see your phone and wallet on the desk.\nThere is a book shelf with books in and an empty beer bottle.\nYou look towards your left and you can see that the window is closed.\nTo your right is you wardrobe which is closed, and that your sink is empty.\nOn the other side of the wardrobe is the door out to the communal hallway.\n")
-
-action = input("\nWhere would you like to go next?\n-->").lower()
-
-end
-
-
-
+print ("\:\: END OF CODE \:\:")
 
 #CURRENT FUNCIONAL POINT ---------------------------------------------------------------------#
 
